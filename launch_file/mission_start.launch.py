52215679fb146569953fa11ee80d8c2ac1392e24
Izen9835 <e1406600@u.nus.edu>
2025-04-09 00:41:48 +0800

    added FSM and necessary topics for information and control over casualty_location, casualty_save, nav2_wfd

--- launch_file/mission_start.launch.py
+++ launch_file/mission_start.launch.py
@@ -8,9 +8,8 @@ def generate_launch_description():
         # mission control / finite state machine node
         # Node(package="mission_control", executable="mission_start", name="mission_control"),
 
-        # amg8833 driver and casualty locator
+        # casualty locator
         Node(package="casualty_location", executable="casualty_location", name="casualty_explorer"),
-        Node(package="casualty_location", executable="ir_pub", name="ir_driver"),
 
         # frontier exploration node
         Node(package="nav2_wfd", executable="explore", name="frontier_explorer"),
--- mission_control/mission_control.py
+++ mission_control/mission_control.py
@@ -2,10 +2,13 @@ import rclpy
 from rclpy.node import Node
 
 # service for control of exploration node
-from custom_msg_srv.srv import StartExploration
+from custom_msg_srv.msg import MapExplored
 
-# for launcher service
-from std_srvs.srv import Trigger 
+# for control of casualty_location node
+from custom_msg_srv.srv import StartCasualtyLocation
+
+# for updating when all casualties found
+from custom_msg_srv.msg import CasualtyLocateStatus
 
 
 class MissionControl(Node):
@@ -13,36 +16,94 @@ class MissionControl(Node):
         super().__init__('mission_control')
         self.get_logger().info("Mission Control Node Started")
 
-        # sub to explorer service for start exploration
-        self.exploration_service = self.create_client(StartExploration, 'start_exploration')
+        # service to set casualty_locate or casualty_save to start
+        self.exploration_service = self.create_client(StartCasualtyLocation, 'casualty_state')
+        self.set_casualty_service("STOPPED")
 
-        # sub to launcher service
-        self.launcher_service = self.create_client(Trigger, 'launch_ball')
+        # topic to tell when nav2_wfd explorer exploration complete
+        # we assume that the exploration node will start by default when the launch file is run
+        self.exploration_topic = self.create_subscription(MapExplored, 'exploration_complete', self.exploration_callback, 10)
+        self.mapExplored = False
 
-        self.mission_start()
+        # topic to tell when casualty_location is done
+        self.casualty_locate = self.create_subscription(CasualtyLocateStatus, 'casualty_found', self.casualty_callback, 10)
+        self.casualties_found = False
 
-    def toggle_exploration(self, explore_now):
+        # topic to tell when all casualty_save is complete
+        self.casualty_save = self.create_subscription(CasualtySaveStatus, 'casualty_saved', self.casualty_save_callback, 10)
+        self.casualties_saved = False
+
+        # prevState to prevent excessive calling of the same state actions
+        self.state = "explore"
+        self.prevState = None
+
+
+        # timer to run the FSM
+        self.create_timer(1.0, self.run_fsm())
+
+
+    def set_casualty_service(self, state):
         self.req = StartExploration.Request()
-        self.req.explore_now = explore_now
+        self.req.state = state
         while not self.exploration_service.wait_for_service(timeout_sec=5.0):
             self.get_logger().warning('explorer not avail, waiting...')
         self.exploration_service.call_async(self.req)
 
+    def exploration_callback(self, msg):
+        self.mapExplored = msg.explore_complete
 
-    def launch_now(self):
-        self.get_logger().info("calling launcher")
-        self.req = Trigger.Request()
-        while not self.launcher_service.wait_for_service(timeout_sec=5.0):
-            self.get_logger().warning('launcher not avail, waiting...')
-        self.launcher_service.call_async(self.req)
+    def casualty_callback(self, msg):
+        self.casualties_found = msg.all_casualties_found
+
+    def casualty_save_callback(self, msg):
+        self.casualties_saved = msg.all_casualties_saved
 
 
-    def mission_start(self):
-        self.get_logger().info("Starting mission...")
-        # Here you would typically initialize your explorer node
-        # self.explorer_node = ExplorerNode()
-        # self.explorer_node.start_exploration()
-        self.toggle_exploration(explore_now=True)
+
+    # FSM: finite state machine
+    def run_fsm(self):
+
+
+        if self.state == 'explore':
+            self.prevState = self.state
+            # wait for exploration to finish
+            if self.mapExplored:
+                self.get_logger().info("exploration complete")
+                self.state = 'locate casualty'
+
+
+
+        elif self.state == 'locate casualty':
+            # ensure that the service is called only once
+            if self.prevState != 'locate casualty':
+                self.set_casualty_service("LOCATE")
+                self.prevState = self.state
+
+            if self.casualties_found:
+                self.get_logger().info("all casualties found")
+                self.state = 'save casualty'
+
+
+
+        elif self.state == 'save casualty':
+            # ensure service only called once
+            if self.prevState != 'save casualty':
+                self.set_casualty_service("SAVE")
+                self.prevState = self.state
+
+            if self.casualties_saved:
+                self.set_casualty_service("STOPPED")
+                self.state = 'mission complete'
+
+
+
+        elif self.state == 'mission complete':
+            if self.prevState != 'mission complete':
+                self.get_logger().info("Mission Completed")
+                self.prevState = self.state
+                # stop the mission control node 
+
+        
             
 
 def main():
