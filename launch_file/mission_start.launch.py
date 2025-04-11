9fb3c3c42bb20994771a02adb07b3556546aeb25
Izen9835 <e1406600@u.nus.edu>
2025-04-12 04:32:04 +0800

    added use_sim_time

--- launch_file/mission_start.launch.py
+++ launch_file/mission_start.launch.py
@@ -1,20 +1,40 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
-
-
+from launch.substitutions import LaunchConfiguration
+from launch.actions import DeclareLaunchArgument
 
 def generate_launch_description():
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
     ld = [
-        # mission control / finite state machine node
-        Node(package="mission_control", executable="missionStart", name="mission_control"),
+        DeclareLaunchArgument('use_sim_time', default_value='true', description='Use simulation time'),
 
-        # casualty locator
-        Node(package="casualty_location", executable="casualty_location", name="casualty_explorer"),
+        Node(
+            package="mission_control",
+            executable="missionStart",
+            name="mission_control",
+            parameters=[{'use_sim_time': use_sim_time}]
+        ),
 
-        # casualty saver
-        Node(package="casualty_location", executable="casualty_saver", name="casualty_saver"),
+        Node(
+            package="casualty_location",
+            executable="casualty_location",
+            name="casualty_explorer",
+            parameters=[{'use_sim_time': use_sim_time}]
+        ),
 
-        # frontier exploration node
-        Node(package="nav2_wfd", executable="explore", name="frontier_explorer"),
+        Node(
+            package="casualty_location",
+            executable="casualty_saver",
+            name="casualty_saver",
+            parameters=[{'use_sim_time': use_sim_time}]
+        ),
+
+        Node(
+            package="nav2_wfd",
+            executable="explore",
+            name="frontier_explorer",
+        ),
     ]
+
     return LaunchDescription(ld)
